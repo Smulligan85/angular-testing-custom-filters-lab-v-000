4f7988c4dd52df8354f8d12d76950ca73467668f
Smulligan85 <sean.mulligan85@gmail.com>
2016-08-09 19:37:34 -0700

    Add custom filter specs

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -6,6 +6,22 @@ describe('Favorite Food Filter', function () {
 	beforeEach(inject(function ($injector) {
 		$filter = $injector.get('$filter');
 	}));
+    it('should filter on favoriteFood correctly', function () {
+       var mockedList = [{
+           name: 'Sean',
+           favoriteFood: 'Pizza'
+       }, {
+           name: 'Erin',
+           favoriteFood: 'Pasta'
+       }, {
+           name: 'John',
+           favoriteFood: 'Indian'
+       }];
 
+        var results = $filter('favoriteFood')(mockedList, 'Pizza');
+
+        expect(results.length).toBe(1);
+        expect(results[0].name).toBe('Sean');
+    });
 	
 });
